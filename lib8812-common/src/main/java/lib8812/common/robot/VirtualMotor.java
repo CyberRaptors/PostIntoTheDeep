0f1bd82b1e97b30c2b7b5e9430c62bb1093b2374
Carl Furtado <carlzfurtado@gmail.com>
2024-09-15 13:27:54 -0400

    test extension mechanism

--- lib8812-common/src/main/java/lib8812/common/robot/VirtualMotor.java
+++ lib8812-common/src/main/java/lib8812/common/robot/VirtualMotor.java
@@ -2,11 +2,17 @@ package lib8812.common.robot;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareDevice;
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 
-public final class VirtualMotor implements IVirtualHardwareDevice {
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+
+public final class VirtualMotor implements IVirtualHardwareDevice, DcMotorEx {
     public boolean isVirtualDevice() { return true; }
 
     public MotorConfigurationType getMotorType() {
@@ -103,5 +109,100 @@ public final class VirtualMotor implements IVirtualHardwareDevice {
 
     public void close() {
 
+    }
+
+    @Override
+    public void setMotorEnable() {
+
+    }
+
+    @Override
+    public void setMotorDisable() {
+
+    }
+
+    @Override
+    public boolean isMotorEnabled() {
+        return false;
+    }
+
+    @Override
+    public void setVelocity(double angularRate) {
+
+    }
+
+    @Override
+    public void setVelocity(double angularRate, AngleUnit unit) {
+
+    }
+
+    @Override
+    public double getVelocity() {
+        return 0;
+    }
+
+    @Override
+    public double getVelocity(AngleUnit unit) {
+        return 0;
+    }
+
+    @Override
+    public void setPIDCoefficients(RunMode mode, PIDCoefficients pidCoefficients) {
+
+    }
+
+    @Override
+    public void setPIDFCoefficients(RunMode mode, PIDFCoefficients pidfCoefficients) throws UnsupportedOperationException {
+
+    }
+
+    @Override
+    public void setVelocityPIDFCoefficients(double p, double i, double d, double f) {
+
+    }
+
+    @Override
+    public void setPositionPIDFCoefficients(double p) {
+
+    }
+
+    @Override
+    public PIDCoefficients getPIDCoefficients(RunMode mode) {
+        return null;
+    }
+
+    @Override
+    public PIDFCoefficients getPIDFCoefficients(RunMode mode) {
+        return null;
+    }
+
+    @Override
+    public void setTargetPositionTolerance(int tolerance) {
+
+    }
+
+    @Override
+    public int getTargetPositionTolerance() {
+        return 0;
+    }
+
+    @Override
+    public double getCurrent(CurrentUnit unit) {
+        return 0;
+    }
+
+    @Override
+    public double getCurrentAlert(CurrentUnit unit) {
+        return 0;
+    }
+
+    @Override
+    public void setCurrentAlert(double current, CurrentUnit unit) {
+
+    }
+
+    @Override
+    public boolean isOverCurrent() {
+        return false;
     }
 }
