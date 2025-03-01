eab3dcfb43d3f69f31598d43640884f1e39b8fe7
Carl Furtado <carlzfurtado@gmail.com>
2025-02-28 21:34:39 -0500

    add StandardTransferBot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/StandardTransferBot.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.robot;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import java.util.ArrayList;
+
+public class StandardTransferBot {
+	public ArrayList<DcMotorEx> horizontalIntakeLifts;
+	public ArrayList<DcMotorEx> verticalOutLifts;
+
+}
